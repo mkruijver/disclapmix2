2a11fb8114b1547df338b055eef6f9f13bf49d1c
Maarten Kruijver <maarten.kruijver@esr.cri.nz>
2021-01-14 07:23:37 +1300

    Adds basic support for non-standard haplotypes by NA'ing

--- R/disclapmix2.R
+++ R/disclapmix2.R
@@ -4,22 +4,45 @@ check_input_db <- function(x){
   if (!all(sapply(x,is.character))) stop("columns of x should be character vectors")
 }
 
-# x_raw <- readxl::read_excel(system.file("extdata", "South Australia.xlsx", package = "disclapmix2"), col_types = "text", sheet = 2)
+to_int_db <- function(x, one_loci, two_loci){
+  
+  # convert the stripped down db to an integer matrix
+  x_int_one_loci <- matrix(as.integer(x[, one_loci]), nrow=nrow(x))
+  colnames(x_int_one_loci) <- one_loci
+  
+  x_int_two_loci <- matrix(integer(), nrow=nrow(x), ncol=2 * length(two_loci))
+  if(length(two_loci) > 0) colnames(x_int_two_loci) <- paste0(rep(two_loci, each=2),c("(1)","(2)"))
+  
+  for( locus in two_loci){
+    locus_split <- ifelse(is.na(x[,locus]), yes = list(c(NA_character_,NA_character_)), no =strsplit(x[,locus], split = ","))
+    
+    x_int_two_loci[,paste0(locus,c("(1)","(2)"))] <- matrix(as.integer(unlist(locus_split)), nrow = nrow(x), ncol=2,byrow = TRUE)
+  }
+  
+  x_int <- cbind(x_int_one_loci, x_int_two_loci)
+  
+  x_int
+}
+
+# x_raw <- readxl::read_excel(system.file("extdata", "South_Australia.xlsx", package = "disclapmix2"), col_types = "text", sheet = 1)
+# x <- x_raw[-c(1,2)]
 # x <- x_raw[-c(1,2,21,26)]
-# x_raw <- readxl::read_excel(system.file("extdata", "Chinese Han.xlsx", package = "disclapmix2"), col_types = "text")
+# x_raw <- readxl::read_excel(system.file("extdata", "Chinese_Han.xlsx", package = "disclapmix2"), col_types = "text")
 # # x_raw
 # x <- x_raw[rep(seq_len(nrow(x_raw)), times=as.integer(x_raw$Count)),-c(1:3)]
-# include_2_loci=FALSE
+
+# include_2_loci=TRUE
 # verbose=1
 # number_of_clusters = 2L
+# remove_non_standard_haplotypes = FALSE
 
 #' @export
-disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, verbose=0L){
+disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, remove_non_standard_haplotypes = TRUE, verbose=0L){
   
   check_input_db(x)
   
   if (verbose>=1) disclapmix2:::verbose_print("Determining loci with suitable data")
-  x_summarised <- summarise_db(x)
+  x_summarised <- disclapmix2::summarise_db(x)
   
   # clean the data such that 1-loci only have 1 integer or NA
   # and 2-loci only have 2 integers or NA
@@ -39,42 +62,64 @@ disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, verbose=0
   x_cleaned <- matrix(data = character(), nrow = nrow(x), ncol = number_of_loci)
   colnames(x_cleaned) <- c(one_loci, if(number_of_2_loci>0) two_loci else NULL)
   
+  if (verbose>=1) disclapmix2:::verbose_print("Setting any haplotype that is not 1 or 2 integers at the approriate locus to NA")
+  cleaned_dfs <- list()
+  
+  # locus = "DYS458"
+  
   for(locus in one_loci){
     ind_1 <- x_summarised$x_ind_12_other[,locus]=="1"
     x_cleaned[ind_1,locus] <- x[[locus]][ind_1]
+    
+    number_removed <- sum(!ind_1)
+    cleaned_dfs[[1+length(cleaned_dfs)]] <- data.frame(row = which(!ind_1), locus=rep(locus, number_removed), haplotype=x[[locus]][!ind_1])
   }
   
   if(number_of_2_loci > 0){
     for(locus in two_loci){
       ind_2 <- x_summarised$x_ind_12_other[,locus]=="2"
       x_cleaned[ind_2,locus] <- x[[locus]][ind_2]
+      
+      number_removed <- sum(!ind_2)
+      cleaned_dfs[[1+length(cleaned_dfs)]] <- data.frame(row = which(!ind_2), locus=rep(locus, number_removed), haplotype=x[[locus]][!ind_2])
     }
   }
   
-  if (verbose>=1) disclapmix2:::verbose_print("Removing profiles with any haplotype that is not 1 or 2 integers at the approriate locus")
-  # for the first stage estimation, we remove any profiles with NAs
-  idx_included <- rowSums(is.na(x_cleaned))==0
-  
-  # create a stripped down version of the db with only the easy profiles
-  x_stripped <- x_cleaned[idx_included,]
-  
-  # convert the stripped down db to an integer matrix
-  x_int_one_loci <- matrix(as.integer(x_stripped[, one_loci]), nrow=nrow(x_stripped))
-  colnames(x_int_one_loci) <- one_loci
-  
-  x_int_two_loci <- matrix(integer(), nrow=nrow(x_stripped), ncol=2 * number_of_2_loci)
-  if(number_of_2_loci > 0) colnames(x_int_two_loci) <- paste0(rep(two_loci, each=2),c("(1)","(2)"))
-  
-  for( locus in two_loci){
-    x_int_two_loci[,paste0(locus,c("(1)","(2)"))] <- matrix(as.integer(unlist(strsplit(x_stripped[,locus], split = ","))), nrow = nrow(x_stripped),ncol=2,byrow = TRUE)
+  cleaned <- do.call(rbind, cleaned_dfs)
+  if (verbose>=1){
+    if (nrow(cleaned>1)){
+      disclapmix2:::verbose_print("Set", nrow(cleaned), "haplotypes to NA:")
+      print(cleaned)
+    }
+    else{
+      disclapmix2:::verbose_print("All haplotypes are integer valued")
+    }
   }
   
-  x_int <- cbind(x_int_one_loci, x_int_two_loci)
+  # create a stripped down version of the db with only the easy profiles
+  x_stripped <- x_cleaned[rowSums(is.na(x_cleaned))==0,]
+  x_stripped_int <- to_int_db(x_stripped, one_loci, two_loci)
   
-  if (verbose>=1) disclapmix2:::verbose_print(nrow(x_int) , paste0("profiles left (started with ", nrow(x),")"))
+  if (remove_non_standard_haplotypes){
+    if (verbose>=1) disclapmix2:::verbose_print("Removing profiles with any haplotype that is not 1 or 2 integers at the approriate locus")
+
+    x_int <- x_stripped_int
+  }else{
+    if (verbose>=1) disclapmix2:::verbose_print("Not removing profiles with any haplotype that is not 1 or 2 integers at the approriate locus")
+    x_int <- to_int_db(x_cleaned, one_loci, two_loci)
+    
+    if (verbose>=1) disclapmix2:::verbose_print(sum(is.na(x_int)), "NA observations in x_int")
+  }
+  
+  if (verbose>=1) disclapmix2:::verbose_print(nrow(x_int) , paste0("profiles used in analysis (started with ", nrow(x),")"))
   if (verbose>=1) disclapmix2:::verbose_print("Determining initial clustering using PAM")
   
   initial_pam <- cluster::pam(x_int, k = number_of_clusters, metric = "manhattan",diss = FALSE, keep.diss=FALSE, keep.data=TRUE)
+  
+  if (anyNA(initial_pam$medoids)){
+    if (verbose>=1) disclapmix2:::verbose_print("NAs in initial clustering, retrying with stripped data")
+    initial_pam <- cluster::pam(x_stripped_int, k = number_of_clusters, metric = "manhattan",diss = FALSE, keep.diss=FALSE, keep.data=TRUE)
+  }
   
   initial_pam_tau <- (tabulate(initial_pam$clustering, nbins = number_of_clusters)/nrow(x_int))
   initial_pam_theta_tau <- if(number_of_clusters==1L) numeric() else log( initial_pam_tau[1:(number_of_clusters-1)])
@@ -108,10 +153,7 @@ disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, verbose=0
     opt <- stats::optim(par = theta, fn = f, method = "BFGS", control = list(reltol = 1e-16))
     if (opt$convergence != 0) stop("BFGS failed to converge")
     
-    # dput(opt$value)
-    
     theta_opt <- opt$par
-    
     theta_iterations[[1+length(theta_iterations)]] <- theta_opt
     
     tau_opt <- disclapmix2:::get_tau(theta = theta_opt, number_of_loci = number_of_loci, number_of_clusters = number_of_clusters)
@@ -124,7 +166,7 @@ disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, verbose=0
     v_matrix <- disclapmix2:::compute_posterior_cluster_prs(profile_pr = x_int_profile_pr_by_cluster, tau = tau_opt)
     
     # see if we need to move centers
-    y_new <- disclapmix:::move_centers(x_int, y, v_matrix)
+    y_new <- disclapmix2:::move_centers(x_int, y, v_matrix)
     
     theta <- theta_opt
     
@@ -176,3 +218,4 @@ disclapmix2 <- function(x, number_of_clusters, include_2_loci = FALSE, verbose=0
     y = y, p = p_opt, tau = tau_opt
   )
 }
+
