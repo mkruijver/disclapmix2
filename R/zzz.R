3c7e951c924ad5bd1f3fa282c5c199995a77c4f2
mkruijver <maarten.kruijver@esr.cri.nz>
2023-04-12 11:48:35 +1200

    Clean up NAMESPACE

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,3 @@
-#' disclapmix2
-#' 
-#' Description of your package
-#' 
-#' @docType package
-#' @author you 
-#' @importFrom Rcpp evalCpp
+#' @importFrom Rcpp evalCpp sourceCpp
 #' @useDynLib disclapmix2
-#' @name disclapmix2
-NULL  
+NULL
